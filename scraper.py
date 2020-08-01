a06c32d137fd4e4845dfa69eb91a1066f6b9e155
mikeku <mikekuforgit@gmail.com>
2020-08-01 20:18:43 +0800

    完成存入MySQL資料庫方法(Method)

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import pymysql
 
 
 class Stock:
@@ -25,3 +26,44 @@ class Stock:
             result.append((stock_date,) +
                           tuple(td.getText().strip() for td in tds))
         return result
+
+    def save(self, stocks):
+
+        db_settings = {
+            "host": "127.0.0.1",
+            "port": 3306,
+            "user": "root",
+            "password": "******",
+            "db": "stock",
+            "charset": "utf8"
+        }
+
+        try:
+            conn = pymysql.connect(**db_settings)
+
+            with conn.cursor() as cursor:
+                sql = """INSERT INTO market(
+                                market_date,
+                                stock_name,
+                                market_time,
+                                final_price,
+                                buy_price,
+                                sell_price,
+                                ups_and_downs,
+                                lot,
+                                yesterday_price,
+                                opening_price,
+                                highest_price,
+                                lowest_price)
+                         VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+
+                for stock in stocks:
+                    cursor.execute(sql, stock)
+                conn.commit()
+
+        except Exception as ex:
+            print("Exception:", ex)
+
+
+stock = Stock('2451', '2454')
+stock.save(stock.scrape())
